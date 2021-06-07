01f4693d4df5c4817e3cf0a013baeb522e654177
Kimon Protopapas <kimon.protopapas@epfl.ch>
2021-06-07 03:31:59 +0200

    basic training

--- train_swisscube.py
+++ train_swisscube.py
@@ -25,14 +25,14 @@ import networks
 from networks import FlowNetS
 
 CUDA_DEVICE = 0
+MINIBATCH_SIZE = 64
 
 
-def load_network():
-
-    network_path = 'data/checkpoints/ycb_object/flownets_ycb_object_20objects_color_self_supervision_epoch_10.checkpoint.pth'
+def load_network(network_path):
 
     weights = torch.load(network_path)
-    network = FlowNetS(1, weights).cuda(device=CUDA_DEVICE)
+    network = FlowNetS(1).cuda(device=CUDA_DEVICE)
+    network.load_state_dict(weights['state_dict'])
     network = torch.nn.DataParallel(network, device_ids=[CUDA_DEVICE]).cuda(device=CUDA_DEVICE)
     cudnn.benchmark = True
 
@@ -46,27 +46,40 @@ def load_poses(path):
     return poses
 
 
-def extract_pose(rotation_list, translation_list):
-    rotation = np.array(rotation_list).reshape((3, 3))
-    translation = np.array(translation_list)
+def extract_pose(pose_dict):
+    rotation = np.array(pose_dict['cam_R_m2c']).reshape((3, 3))
+    translation = np.array(pose_dict['cam_t_m2c'])
 
     quaternion = R.from_matrix(rotation).as_quat()
-
     pose = np.concatenate((quaternion, translation))  # TODO: CHECK!!!!!!!!!!!!!!
 
     return pose
 
-def generate_samples():
 
-    path = '/cvlabdata2/cvlab/datasets_protopap/swisscube'
-    folders = os.listdir(path)
-    for folder in folders:
-        sub_folder = folder[folder.index('_') + 1:]
-        full_path = os.path.join(path, folder, sub_folder)
-        poses = load_poses(full_path)
-        images_path = os.path.join(full_path, 'rgb')
-        for image_name in os.listdir(images_path):
-            poses = poses[]
+def generate_samples(split='testing'):
 
+    if split not in ['training', 'validation', 'testing']:
+        print(f'invalid split name {split}')
+        exit(-1)
 
-        yield sample
+    points = None
+    weights_rot = np.array([1, 1, 1, 1])
+    extents = None
+
+    images_cuda = torch.FloatTensor(MINIBATCH_SIZE, 6, width, height).cuda()
+    flow_cuda = torch.FloatTensor(MINIBATCH_SIZE, 4, width, height).cuda()
+
+    base_path = '/cvlabdata2/cvlab/datasets_protopap/swisscube/'
+
+    images_list_path = os.path.join(base_path, f'{split}.txt')
+    with open(images_list_path, 'r') as f:
+        images_list = f.readlines()
+
+    for img_path in images_list:
+        full_path = os.path.join(base_path, img_path)
+        pose_path = os.path.join(*os.path.split(full_path)[:-2], 'scene_gt.json')
+        poses = load_poses(pose_path)
+        pose = extract_pose(poses[str(int(os.path.basename(full_path)))])  # TODO check again!!!!!!!
+
+        img = cv2.imread(full_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
