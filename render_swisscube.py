3852fbb1d488773213c0d807d59df5748a336117
Kimon Dimitrios Protopapas <kimon.protopapas@epfl.ch>
2021-06-09 14:21:08 +0200

    fix render angle

--- render_swisscube.py
+++ render_swisscube.py
@@ -87,18 +87,31 @@ def quaternion2rotation(quat):
 
 
 class Renderer:
-    def __init__(self):
+    def __init__(self, synthetic=False):
+        self.synthetic = synthetic
         os.environ['PYOPENGL_PLATFORM'] = 'egl'
         tscene = trimesh.load('/cvlabdata2/cvlab/datasets_protopap/deepim/data/models/swisscube/swisscube.obj')
         mesh = pyrender.Mesh.from_trimesh(list(tscene.geometry.values()), smooth=False)
 
-        self.renderer = pyrender.OffscreenRenderer(viewport_width=2048, viewport_height=2048, point_size=1.0)
+        if synthetic:
+            width, height = 1024, 1024
+        else:
+            width, height = 2048, 2048
+
+
+        self.renderer = pyrender.OffscreenRenderer(viewport_width=width, viewport_height=height, point_size=1.0)
         scene = pyrender.Scene(ambient_light=[0.02, 0.02, 0.02], bg_color=[0, 0, 0])
 
         light = pyrender.PointLight(color=[1.0, 1.0, 1.0], intensity=1000000.0)
         
-        self.intrinsic = np.array([4000, 0, 1024, 0, 4000, 1024, 0, 0, 1]).reshape((3, 3))
-        cam = pyrender.IntrinsicsCamera(4000, 4000, 1024, 1024, zfar=2000)
+        if synthetic:
+            fx, fy, cx, cy = 607, 607, 512, 512
+        else:
+            fx, fy, cx, cy = 4000, 4000, 1024, 1024
+
+
+        self.intrinsic = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1]).reshape((3, 3))
+        cam = pyrender.IntrinsicsCamera(fx, fy, cx, cy, zfar=2000)
         cam_rot = R.from_euler('y', 180, degrees=True).as_matrix()
         cam_matrix = to_homo(cam_rot, np.zeros((3,)))
 
@@ -127,15 +140,17 @@ class Renderer:
     def set_pose(self, pose):
         translation, rotation_quat = pose[:3], pose[3:]
         translation = np.array(translation)
+        if not self.synthetic:
+            rotation = quaternion2rotation(rotation_quat)
+        else:
+            rotation = R.from_quat(rotation_quat).as_matrix()
+            rotation = rotation @ R.from_euler('x', 90, degrees=True).as_matrix()
 
-        # rotation = R.from_quat(rotation_quat).as_matrix()
-        rotation = quaternion2rotation(rotation_quat)
         transform = to_homo(rotation, translation)
         self.scene.set_pose(self.nm, pose=transform)
 
     def render_(self):
         color, depth = self.renderer.render(self.scene)
-        
         color = cv2.resize(color, (640, 640), cv2.INTER_AREA)
         color = color[80:560]
         
@@ -161,66 +176,62 @@ class Renderer:
             pc2_tensor.copy_(image_tensor)
 
 
-def get_next(iteritems):
-    
-    
-    img, pose = next(iteritems)
-    img = os.path.join(img.split('/')[0], 'Test', img.split('/')[1])
-    img = cv2.imread(img)
-    img = cv2.resize(img, (640, 640), cv2.INTER_AREA)
-    img = img[80:560]
-    rotation = pose['rotation_m2c']
-    translation = pose['translation_m2c']
-    """
+    def get_next(self, iteritems):
+         
+        if not self.synthetic:
+            img, pose = next(iteritems)
+            img = os.path.join(img.split('/')[0], 'Test', img.split('/')[1])
+            img = cv2.imread(img)
+            img = cv2.resize(img, (640, 640), cv2.INTER_AREA)
+            img = img[80:560]
+            rotation = pose['rotation_m2c']
+            translation = pose['translation_m2c']
+            
+        else:
+            img_path = next(iteritems).strip()
+            full_path = os.path.join('/cvlabdata2/home/yhu/data/SwissCube_1.0', img_path)
+            num = str(int(os.path.splitext(os.path.basename(full_path))[0]))
+            img = cv2.imread(full_path)
+            img = cv2.resize(img, (640, 640), cv2.INTER_AREA)
+            img = img[80:560]
+            seq_name = os.path.dirname(os.path.dirname(full_path))
+            
+            poses_name = os.path.join(seq_name, 'scene_gt.json')
+            with open(poses_name, 'r') as j:
+                poses = json.load(j)
 
-    
-    img_path = next(iteritems).strip()
-    full_path = os.path.join('/cvlabdata2/home/yhu/data/SwissCube_1.0', img_path)
-    num = str(int(os.path.splitext(os.path.basename(full_path))[0]))
-    img = cv2.imread(full_path)
+            
+            pose = poses[num][0]
+            translation = np.array(pose['cam_t_m2c'])
+            rotation = np.array(pose['cam_R_m2c']).reshape((3, 3))
+            rotation = R.from_matrix(rotation).as_quat()
+            
 
-    seq_name = os.path.dirname(os.path.dirname(full_path))
-    
-    poses_name = os.path.join(seq_name, 'scene_gt.json')
-    with open(poses_name, 'r') as j:
-        poses = json.load(j)
-
-    
-    pose = poses[num][0]
-    translation = np.array(pose['cam_t_m2c'])
-    rotation = np.array(pose['cam_R_m2c']).reshape((3, 3))
-    rotation = R.from_matrix(rotation).as_quat()
-    """
-
-    return img, translation, rotation
+        return img, translation, rotation
 
 
 if __name__ == '__main__':
-    r = Renderer()
+    r = Renderer(True)
 
     import os
     import json
     
-    """
-    os.chdir('/cvlabdata2/home/yhu/data/SwissCube_1.0')
-    with open('testing.txt', 'r') as f:
-        images = f.readlines()
+    if r.synthetic:
+        os.chdir('/cvlabdata2/home/yhu/data/SwissCube_1.0')
+        with open('testing.txt', 'r') as f:
+            images = f.readlines()
 
-    iteritems = iter(images)
+        iteritems = iter(images)
+        
+    else:
+        os.chdir('/cvlabdata2/cvlab/datasets_protopap/SwissCubeReal')
+        with open('data.json', 'r') as f:
+            poses = json.load(f)
+
+        iteritems = iter(poses.items())
     
-    """
-    os.chdir('/cvlabdata2/cvlab/datasets_protopap/SwissCubeReal')
-    with open('data.json', 'r') as f:
-        poses = json.load(f)
-
-    iteritems = iter(poses.items())
-    
-
-    img, translation, rotation = get_next(iteritems)
+    img, translation, rotation = r.get_next(iteritems)
     translation = np.array(translation)
-    old_rotation = quaternion2rotation(rotation)
-    mesh = trimesh.load('/cvlabdata2/cvlab/datasets_protopap/SwissCubeReal/obj_000001.ply')
-    img = add_pose_contour(mesh, r.intrinsic, old_rotation, translation, (0, 255, 0), img, 2048 / 640)
     cv2.imshow('image', img)
     x, y, z = translation
 
@@ -235,14 +246,10 @@ if __name__ == '__main__':
         if key == 27:
             break
         elif key == 13:
-            img, translation, old_rotation = get_next(iteritems)
-            rotation = old_rotation
+            img, translation, rotation = r.get_next(iteritems)
             x, y, z = translation
             a, b, c, d = rotation
            
-            old_rotation = quaternion2rotation(rotation)
-            translation = np.array(translation)
-            img = add_pose_contour(mesh, r.intrinsic, old_rotation, translation, (0, 255, 0), img, 2048 / 640)
             cv2.imshow('image', img)
         elif key == 119:
             y += 10
--- train_swisscube.py
+++ train_swisscube.py
@@ -24,6 +24,10 @@ from datasets.factory import get_dataset
 import networks
 from networks import FlowNetS
 
+from render_swisscube import Renderer
+from fcn.train_test import process_sample, _compute_pose_target
+from fcn.multiscaleloss import multiscaleEPE, realEPE
+
 CUDA_DEVICE = 0
 MINIBATCH_SIZE = 64
 
@@ -39,6 +43,46 @@ def load_network(network_path):
     return network
 
 
+
+def init_tensors():
+
+
+    num = MINIBATCH_SIZE
+    height, width = 480, 640
+    input_blob_color = torch.cuda.FloatTensor(num, 6, height, width).detach()
+    image_real_blob_color = torch.cuda.FloatTensor(num, 3, height, width).detach()
+    image_tgt_blob_color = torch.cuda.FloatTensor(num, 3, height, width).detach()
+    image_src_blob_color = torch.cuda.FloatTensor(num, 3, height, width).detach()
+    input_blob_depth = torch.cuda.FloatTensor(num, 6, height, width).detach()
+    image_real_blob_depth = torch.cuda.FloatTensor(num, 3, height, width).detach()
+    image_tgt_blob_depth = torch.cuda.FloatTensor(num, 3, height, width).detach()
+    image_src_blob_depth = torch.cuda.FloatTensor(num, 3, height, width).detach()
+    affine_matrices = torch.cuda.FloatTensor(num, 2, 3).detach()
+    zoom_factor = torch.cuda.FloatTensor(num, 4).detach()
+    flow_blob = torch.cuda.FloatTensor(num, 2, height, width).detach()
+    pcloud_tgt_cuda = torch.cuda.FloatTensor(height, width, 3).detach()
+    pcloud_src_cuda = torch.cuda.FloatTensor(height, width, 3).detach()
+    flow_map_cuda = torch.cuda.FloatTensor(height, width, 2).detach()
+
+    train_data = {'input_blob_color': input_blob_color,
+                  'image_real_blob_color': image_real_blob_color,
+                  'image_tgt_blob_color': image_tgt_blob_color,
+                  'image_src_blob_color': image_src_blob_color,
+                  'input_blob_depth': input_blob_depth,
+                  'image_real_blob_depth': image_real_blob_depth,
+                  'image_tgt_blob_depth': image_tgt_blob_depth,
+                  'image_src_blob_depth': image_src_blob_depth,
+                  'affine_matrices': affine_matrices,
+                  'zoom_factor': zoom_factor,
+                  'flow_blob': flow_blob,
+                  'pcloud_tgt_cuda': pcloud_tgt_cuda,
+                  'pcloud_src_cuda': pcloud_src_cuda,
+                  'flow_map_cuda': flow_map_cuda}
+
+
+    return train_data
+
+
 def load_poses(path):
     with open(path, 'r') as f:
         poses = json.load(f)
@@ -52,7 +96,8 @@ def extract_pose(pose_dict):
 
     quaternion = R.from_matrix(rotation).as_quat()
     pose = np.concatenate((quaternion, translation))  # TODO: CHECK!!!!!!!!!!!!!!
-
+    pose_deepim = np.zeros((1, 9))
+    pose_deepim[2:] = pose
     return pose
 
 
@@ -66,6 +111,7 @@ def generate_samples(split='testing'):
     weights_rot = np.array([1, 1, 1, 1])
     extents = None
 
+    width, height = 640, 480
     images_cuda = torch.FloatTensor(MINIBATCH_SIZE, 6, width, height).cuda()
     flow_cuda = torch.FloatTensor(MINIBATCH_SIZE, 4, width, height).cuda()
 
@@ -75,12 +121,111 @@ def generate_samples(split='testing'):
     with open(images_list_path, 'r') as f:
         images_list = f.readlines()
 
+    images = []
+    poses_est = []
     for img_path in images_list:
-        full_path = os.path.join(base_path, img_path)
-        pose_path = os.path.join(*os.path.split(full_path)[:-2], 'scene_gt.json')
+        full_path = os.path.join(base_path, img_path.strip())
+        pose_path = os.path.join(os.path.dirname(os.path.dirname(full_path))), 'scene_gt.json')
         poses = load_poses(pose_path)
         pose = extract_pose(poses[str(int(os.path.basename(full_path)))])  # TODO check again!!!!!!!
 
         img = cv2.imread(full_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (640, 640), cv2.INTER_AREA)
+        img = img[80:560]
+        img = np.transpose(img, (2, 1, 0))[np.newaxis, :]
+        images.append(img)
 
+
+        if len(images) == MINIBATCH_SIZE:
+            images_cpu = np.vstack(images)
+            images_cuda = torch.from_numpy(images_cpu).cuda()
+            
+
+            poses_est.append(pose)
+            sample = {'image_color': images_cuda,
+                  'image_depth': images_cuda,
+                  'meta_data': torch.from_numpy(meta_data_blob[np.newaxis,:]),
+                  'label_blob': torch.from_numpy(label_blob[np.newaxis,:]),
+                  'poses': torch.from_numpy(poses_est),
+                  'extents': torch.from_numpy(dataset._extents[np.newaxis,:]),
+                  'points': torch.from_numpy(dataset._point_blob[np.newaxis,:]),
+                  'poses_result': torch.from_numpy(poses_est[np.newaxis,:])}
+            yield sample
+
+def train(gen_samples, network, optimizer):
+    
+    train_data = init_tensors()
+    for sample in gen_samples:
+       
+        end = time.time()
+        inputs, inputs_depth, flow, poses_src, poses_tgt, \
+            weights_rot, extents, points, affine_matrices, zoom_factor, vdata = \
+            process_sample(sample, poses_est, train_data)
+        data_time.update(time.time() - end)
+
+        # measure data loading time
+        poses_src = poses_src.cuda().detach()
+        poses_tgt = poses_tgt.cuda().detach()
+        weights_rot = weights_rot.cuda().detach()
+        extents = extents.cuda().detach()
+        points = points.cuda().detach()
+
+        # zoom in image
+        grids = nn.functional.affine_grid(affine_matrices, inputs.size())
+        input_zoom = nn.functional.grid_sample(inputs, grids).detach()
+
+        # zoom in flow
+        flow_zoom = nn.functional.grid_sample(flow, grids)
+        for k in range(flow_zoom.shape[0]):
+            flow_zoom[k, 0, :, :] /= affine_matrices[k, 0, 0] * 20.0
+            flow_zoom[k, 1, :, :] /= affine_matrices[k, 1, 1] * 20.0
+        
+        output, loss_pose_tensor, quaternion_delta_var, translation_var = \
+            network(input_zoom, weights_rot, poses_src, poses_tgt, extents, points, zoom_factor)
+
+        
+        vdata_pose = vdata['pose_src']
+        quaternion_delta = quaternion_delta_var.cpu().detach().numpy()
+        translation = translation_var.cpu().detach().numpy()
+        poses_est, error_rot, error_trans = \
+                _compute_pose_target(quaternion_delta, translation, vdata_pose, vdata['pose_tgt'])
+
+        # losses
+        loss_pose = torch.mean(loss_pose_tensor)
+        loss_flow = 0.1 * multiscaleEPE(output, flow_zoom)
+        flow2_EPE = realEPE(output[0], flow_zoom)
+        loss = loss_pose + loss_flow
+
+        # compute gradient and do optimization step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time = time.time() - end
+
+        print('epoch: [%d/%d][%d/%d], iter %d, loss %.4f, l_pose %.4f (%.2f, %.2f), l_flow %.4f, lr %.6f, data time %.2f, batch time %.2f' \
+            % (epoch, cfg.epochs, i, epoch_size, j+1, loss, loss_pose, error_rot, error_trans, loss_flow, \
+              optimizer.param_groups[0]['lr'], data_time.val, batch_time.val))
+
+                
+                
+if __name__ == '__main__':
+    
+    cfg_from_file('experiments/cfgs/swisscube.yml')
+
+    cfg.renderer = Renderer(synthetic=True)
+    network_path = 'data/checkpoints/from_deepim.pth'
+    network = load_network(network_path)
+    
+    
+    param_groups = network.params() # TODO fix
+    optimizer = torch.optim.SGD(param_groups, cfg.TRAIN.LEARNING_RATE, momentum=cfg.TRAIN.MOMENTUM)
+    
+
+
+    epochs = 25
+    cfg.epochs = epochs
+    for i in range(epochs):
+        train(generate_samples('training'), network, optimizer)
